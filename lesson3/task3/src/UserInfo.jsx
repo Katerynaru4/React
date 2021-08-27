a8f67aeb2dbceb9ff72973e6720c655f856fc5fd
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-08-28 00:20:02 +0300

    components

--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -4,7 +4,7 @@ import Avatar from './Avatar';
 const UserInfo = (props) => {
   return (
     <div className="user-info">
-      <Avatar user={props} />
+      <Avatar user={props.user} />
       <div className="user-info__name">{props.user.name}</div>
     </div>
   );
