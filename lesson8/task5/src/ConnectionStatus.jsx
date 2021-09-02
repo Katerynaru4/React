8f2e8960d39dc762fe021b9c3220bb14ba586a55
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-03 02:19:20 +0300

    Network connection

---
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class ConnectionStatus extends Component {
+  state = {
+    status: 'online',
+  };
+
+  componentDidMount() {
+    window.addEventListener('offline', () => {
+      this.setState({
+        status: 'offline',
+      });
+    });
+    window.addEventListener('online', () => {
+      this.setState({
+        status: 'online',
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className={`status ${
+          this.state.status === 'offline' && 'status_offline'
+        }`}
+      >
+        Offline
+      </div>
+    );
+  }
+}
+export default ConnectionStatus;
