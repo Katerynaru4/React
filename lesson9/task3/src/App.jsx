f1e3ab707df2d02047ccda0f3ff7b87feaf8b046
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-04 01:28:38 +0300

    Uncontrolled component

---
+++ lesson9/task3/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm.jsx';
+ 
+const App = () => {
+  const createUser = (user) => {
+    console.log(user);
+  };
+  return <UserForm onSubmit={createUser} />;
+};
+
+export default App;
