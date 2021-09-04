374f8a30277c0bc01b4f952f6cade2f146c2d748
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-04 16:56:24 +0300

    State lifting

---
+++ lesson10/task1/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Page from './App.jsx';
+
+
+ReactDOM.render(<Page  userId="github"/>, document.getElementById('root'));
