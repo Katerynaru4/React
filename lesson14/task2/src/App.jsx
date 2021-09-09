29e9bb2ceed76c10c57b3bee87409d44cc41eaf5
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-09 22:41:05 +0300

    react hooks

---
+++ lesson14/task2/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import User from './User.jsx';
+
+const App = () => {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <h1>Users</h1>
+        <BrowserRouter>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <span>Select a user please</span>
+            </Route>
+            <Route path="/users/:userId">
+              <User />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+};
+
+export default App;
